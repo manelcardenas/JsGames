3bd41a198b58a91b5efe28991027ed06fa2c0a8d
manelcardenas <joan.manel.cardenas@estudiantat.upc.edu>
2023-06-24 12:41:35 +0200

    24/06_ajustes_esteticos

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -51,94 +51,114 @@ const cardArray = [
 
 // en cas de perdre, que torni a començcar automaticament
 
-const resultDisplay = document.querySelector('#result')
-const scoreDisplay = document.querySelector('#score')
-const attempsLeftDisplay = document.querySelector('#attemps')
 
-let gridDisplay = document.querySelector('#grid')
-let card
-let attempsId
-let currentattemps = 8
-let cardChosen = []
-let cardsChosenIds = []
-let cardsWon = []
-let partida = 0
+const resultDisplay = document.querySelector('#result');
+const scoreDisplay = document.querySelector('#score');
+const attemptsLeftDisplay = document.querySelector('#attempts');
 
-function partidas () {
-    if(partida === 0){
-        cardArray.sort(() => 0.5 - Math.random())
-        createBoard()
-        partida = 1
-    }
+let gridDisplay = document.querySelector('#grid');
+let card;
+let currentAttempts = 8;
+let cardChosen = [];
+let cardsChosenIds = [];
+let cardsWon = [];
+let partida = 0;
+let gameStarted = false;
+
+function startGame() {
+    cardArray.sort(() => 0.5 - Math.random());
+    createBoard();
+    partida = 1;
+    gameStarted = true;
 }
 
-
-function createBoard() {
-    for(let i=0;i<cardArray.length;i++){
-        card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
-        card.setAttribute('data-id',i)
-        card.addEventListener('click', flipCard)
-        gridDisplay.append(card)
-    }
+function restartGame() {
+    deleteBoard();
+    cardArray.sort(() => 0.5 - Math.random());
+    createBoard();
+    currentAttempts = 8;
+    cardsWon = [];
+    resultDisplay.textContent = cardsWon.length;
+    attemptsLeftDisplay.textContent = currentAttempts;
+    cardChosen = [];
+    cardsChosenIds = [];
+    partida = 1;
+    gameStarted = true;
 }
 
-partidas()
+function deleteBoard() {
+    while (gridDisplay.firstChild) {
+        gridDisplay.removeChild(gridDisplay.firstChild);
+    }
+}
 
 function checkMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOne = cardsChosenIds[0]
-    const optionTwo = cardsChosenIds[1]
+    const cards = document.querySelectorAll('img');
+    const optionOne = cardsChosenIds[0];
+    const optionTwo = cardsChosenIds[1];
 
-    if(optionOne == optionTwo){
-        cards[optionOne].setAttribute('src', 'images/blank.png')
-        cards[optionTwo].setAttribute('src', 'images/blank.png')
-        scoreDisplay.innerHTML = 'You clicked the same card twice!'
-    }else if(cardChosen[0] == cardChosen[1]) {
-        scoreDisplay.innerHTML = 'You found a match'
-        cards[optionOne].setAttribute('src', 'images/white.png')
-        cards[optionTwo].setAttribute('src', 'images/white.png')
-        cards[optionOne].removeEventListener('click', flipCard)
-        cards[optionTwo].removeEventListener('click', flipCard)
-        cardsWon.push(cardChosen)
-        currentattemps++
+    if (optionOne == optionTwo) {
+        cards[optionOne].setAttribute('src', 'images/blank.png');
+        cards[optionTwo].setAttribute('src', 'images/blank.png');
+        scoreDisplay.innerHTML = 'You clicked the same card twice!';
+    } else if (cardChosen[0] == cardChosen[1]) {
+        scoreDisplay.innerHTML = 'You found a match';
+        cards[optionOne].setAttribute('src', 'images/white.png');
+        cards[optionTwo].setAttribute('src', 'images/white.png');
+        cards[optionOne].removeEventListener('click', flipCard);
+        cards[optionTwo].removeEventListener('click', flipCard);
+        cardsWon.push(cardChosen);
+        currentAttempts++;
     } else {
-        cards[optionOne].setAttribute('src', 'images/blank.png')
-        cards[optionTwo].setAttribute('src', 'images/blank.png')
-        scoreDisplay.innerHTML = 'Sorry try again!'
+        cards[optionOne].setAttribute('src', 'images/blank.png');
+        cards[optionTwo].setAttribute('src', 'images/blank.png');
+        scoreDisplay.innerHTML = 'Sorry, try again!';
     }
 
-    currentattemps--
-    attempsLeftDisplay.textContent = currentattemps
-    setTimeout( () => scoreDisplay.innerHTML = '', 1000)
-    resultDisplay.textContent = cardsWon.length
-    cardChosen = []
-    cardsChosenIds = [] 
+    currentAttempts--;
+    attemptsLeftDisplay.textContent = currentAttempts;
+    setTimeout(() => scoreDisplay.innerHTML = '', 1000);
+    resultDisplay.textContent = cardsWon.length;
+    cardChosen = [];
+    cardsChosenIds = [];
 
-    if(cardsWon.length == (cardArray.length)/2) {
-        alert('Congratulations you found them all!')
+    if (cardsWon.length == cardArray.length / 2) {
+        alert('Congratulations, you found them all!');
+        restartGame();
     }
 
-    if(currentattemps <= 0) {
-        scoreDisplay.innerHTML = 'You lose!'
-        delete gridDisplay
-        console.log(typeof(gridDisplay))
-        //gridDisplay = document.querySelector('#grid')
-        currentattemps = 10
-        cardsWon = []
-        resultDisplay.textContent = cardsWon.length
-        attempsLeftDisplay.textContent = currentattemps
-        card = null
-        partida = 0
-    }
+    if (currentAttempts <= 0) {
+        if (confirm('You lose! Do you want to play again?')) {
+          restartGame();
+        } else {
+          gameStarted = false;
+        }
+      }
 }
 
 function flipCard() {
-    const cardId = this.getAttribute('data-id')
-    cardChosen.push(cardArray[cardId].name)
-    cardsChosenIds.push(cardId)
-    this.setAttribute('src', cardArray[cardId].img)
-    if(cardChosen.length === 2){
-        setTimeout(checkMatch, 500)
+    const cardId = this.getAttribute('data-id');
+    cardChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+    this.setAttribute('src', cardArray[cardId].img);
+
+    if (!gameStarted) {
+        return; // Don't allow flipping cards before starting the game
+    }
+
+    if (cardChosen.length === 2) {
+        setTimeout(checkMatch, 500);
     }
 }
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.png');
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
+        gridDisplay.appendChild(card);
+    }
+}
+
+startGame();
--- Whac-a-mole/app.js
+++ Whac-a-mole/app.js
@@ -5,6 +5,8 @@ const cross = document.querySelector('.cross')
 const timeLeft = document.querySelector('#time-left')
 const socre = document.querySelector('#score')
 const startButton = document.querySelector('.start-button')
+const whack = new Audio('whack.mp3')
+const wrong = new Audio('wrong.mp3')
 
 // falla el bang, s'ha de fer el remove d'alguna altre manera
 
@@ -14,6 +16,7 @@ let currentTime = 40
 let randomSquare
 let timerId
 let countMole
+let anterior
 
 function randomSquares() {
     squares.forEach(square => {
@@ -29,16 +32,21 @@ function randomSquares() {
 squares.forEach(square => {
     square.addEventListener('mousedown', () => {
         if(square.id == hitPosition){
-            randomSquare.classList.add('bang')
+            anterior = randomSquare.classList
+            whack.currentTime = 0
+            whack.play()
+            anterior.add('bang')
             result++
             socre.textContent = result
             hitPosition = null
         } else if(square.id != hitPosition){
             square.classList.add('cross')
+            wrong.currentTime = 0
+            wrong.play()
             setTimeout( () => square.classList.remove('cross'), 200)
             currentTime = currentTime - 2
         }
-        setTimeout( () => randomSquare.classList.remove('bang'), 250)
+        setTimeout( () => anterior.remove('bang'), 250)
     })
 })
 
